8f6203a3bed17da48ad7983ede964d12f0641a10
hellowyh <wangyiheng@ddjf.com.cn>
2019-03-23 23:14:28 +0800

    增加欢迎页面

--- miniprogram/pages/core/upload/index.js
+++ miniprogram/pages/core/upload/index.js
@@ -1,4 +1,8 @@
 // pages/core/upload/index.js
+import regeneratorRuntime from '../../../libs/runtime.js'
+// 初始化数据库
+const db = wx.cloud.database()
+
 Page({
 
   /**
@@ -9,6 +13,52 @@ Page({
   },
 
   chooseImg() {
-    
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['compressed'],
+      sourceType: ['album', 'camera'],
+      success: function (res) {
+        wx.showLoading({
+          title: '上传中',
+        })
+        const filePath = res.tempFilePaths[0]
+        // 上传图片
+        const cloudPath = 'my-image' + filePath.match(/\.[^.]+?$/)[0]
+        // 云服务器上传文件
+        wx.cloud.uploadFile({
+          cloudPath,
+          filePath,
+          success: res => {
+            console.log('[上传] 成功：', res)
+            // 数据库插入数据
+            db.collection('clothes').add({
+              data: {
+                pic_id: res.fileID,
+                name: '测试名称',
+                createdTime: +new Date(),
+                updateTime: +new Date(),
+                owner_id: '123456', // 改为用户的openid
+                type: '衣服', 
+                tags: ['xxx', 'x'],
+                description: 'heiheihei'
+              },
+              success: res => {
+                console.log(res)
+              }
+            })            
+          },
+          fail: e => {
+            console.error('[上传文件] 失败：', e)
+            wx.showToast({
+              icon: 'none',
+              title: '上传失败',
+            })
+          },
+          complete: () => {
+            wx.hideLoading()
+          }
+        })
+      }
+    })
   }
 })
