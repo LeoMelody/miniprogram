fef8e91680bf34e85ee6b80c575af62f3e04103d
leohengwang <leohengwang@tencent.com>
2019-09-29 09:44:40 +0800

    修改部分信息

---
+++ miniprogram/pages/test/pictest/index.js
@@ -0,0 +1,66 @@
+// pages/test/pictest/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    testPic: "cloud://lmj-fd0acf.6c6d-lmj-fd0acf-1258802693/clothes/1569641721311.jpg"
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
